a9b23d3f02d91a5473abcad90fd96b334a8973ad
MiKinshu <mishraprateekaries@gmail.com>
2019-05-26 03:15:29 +0530

    Made front end for login and signup page

---
+++ app/src/main/java/com/example/android/querymaster/Signup_Activity.java
@@ -0,0 +1,13 @@
+package com.example.android.querymaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_);
+    }
+}
