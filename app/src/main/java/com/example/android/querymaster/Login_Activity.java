cd27d52ec70bc47dac0dedd020e20b40847426b9
MiKinshu <mishraprateekaries@gmail.com>
2019-05-26 03:17:59 +0530

    added front for login and signup activity

--- app/src/main/java/com/example/android/querymaster/Login_Activity.java
+++ app/src/main/java/com/example/android/querymaster/Login_Activity.java
@@ -1,7 +1,11 @@
 package com.example.android.querymaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+        EditText ETusername=findViewById(R.id.ETusername);
+        EditText ETpassword=findViewById(R.id.ETpassword);
+        Button BTNsignin=findViewById(R.id.BTNsignin);
+        Button BTNforgotpassword=findViewById(R.id.BTNforgotpassword);
+        Button BTNsignup=findViewById(R.id.BTNsignup);
+        BTNsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //implementing sign in logic
+                //if user found
+
+            }
+        });
+        BTNsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Login_Activity.this,com.example.android.querymaster.Signup_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/querymaster/Signup_Activity.java
+++ app/src/main/java/com/example/android/querymaster/Signup_Activity.java
@@ -2,6 +2,8 @@ package com.example.android.querymaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Signup_Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Signup_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_);
+        EditText ETusername=findViewById(R.id.ETusername);
+        EditText ETpassword=findViewById(R.id.ETpassword);
+        EditText ETemail=findViewById(R.id.ETemail);
+        Button BTNsignup=findViewById(R.id.BTNsignup);
     }
 }
